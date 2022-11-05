53992b83df678f596f6faffecbe0d02ee127b1f5
greg-chuchro <gchuchro@acm.org>
2022-11-05 20:25:31 +0900

    (#7) foo4 (#8)

--- Ghbvft6.GithubNugetLib/Class1.cs
+++ Ghbvft6.GithubNugetLib/Class1.cs
@@ -13,5 +13,8 @@ namespace Ghbvft6.GithubNugetLib
         public String foo3() {
             return "Ghbvft6.GithubNugetLib3";
         }
+        public String foo4() {
+            return "Ghbvft6.GithubNugetLib4";
+        }
     }
 }
